016efb6b94c555399188d12d0c5bfed642b1c2f6
Kumiko <dinarysf276@gmail.com>
2025-11-26 07:16:21 +0700

    Create semanggi.sh

---
+++ semanggi.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# ok
+rm -rf prebuilts/clang/host/linux-x86
+
+# repo init
+repo init -u https://github.com/The-Clover-Project/manifest.git -b 16-qpr1 --git-lfs 
+
+# Sync repository
+/opt/crave/resync.sh || repo sync
+
+# cloning trees
+git clone https://github.com/SilverEpuhonium/android_device_xiaomi_earth.git -b Clover-16.1 device/xiaomi/earth
+git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23.1 vendor/xiaomi/earth
+git clone https://github.com/SilverEuphonium/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
+
+# Mediatek and Hw repos
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
+git clone https://github.com/SilverEuphpnium/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
+
+# Pelengkap
+git clone https://github.com/SilverEuphonium/priv_keys.git -b clover vendor/clover-priv/keys
+git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
+
+# Export
+export BUILD_USERNAME=kumiko
+export BUILD_HOSTNAME=pangokceria
+export TZ="Asia/Jakarta"
+export ALLOW_MISSING_DEPENDENCIES=true
+
+# starts build setup !
+. build/envsetup.sh
+lunch clover_earth-bp3a-userdebug
+mka bacon
