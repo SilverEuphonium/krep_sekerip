61ef47aa2575c40ec39a94f52268b6a37c9bd2e4
Kumiko <dinarysf276@gmail.com>
2025-12-01 09:39:05 +0700

    Update evo.sh

--- evo.sh
+++ evo.sh
@@ -9,7 +9,7 @@ repo init -u https://github.com/Evolution-X/manifest -b bq1 --git-lfs
 /opt/crave/resync.sh || repo sync
 
 # remove
-rm -rf device/xiaomi/earth
+rm -rf device/xiaomi/earth kernel/xiaomi/earth vendor/xiaomi/earth
 # cloning trees
 git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b EvolutionX-16.1 device/xiaomi/earth
 git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23.1 vendor/xiaomi/earth
