2f512349600fdf4d0fbcbfbdffbc908278d905b0
Kumiko <dinarysf276@gmail.com>
2025-12-02 14:45:04 +0700

    Update matrixx.sh

--- matrixx.sh
+++ matrixx.sh
@@ -4,7 +4,7 @@ rm -rf prebuilts/clang/host/linux-x86
 repo init -u https://github.com/ProjectMatrixx/android.git -b 16.0 --git-lfs
 /opt/crave/resync.sh || repo sync
 
-rm -rf device/xiaomi/earth kernel/xiaomi/earth
+rm -rf device/xiaomi/earth 
 git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b Matrixx-16 device/xiaomi/earth
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.0 vendor/xiaomi/earth
 git clone https://github.com/SilverEuphonium/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
