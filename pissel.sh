325981a556ec90e0b552c809d14d5064b1da9e08
Kumiko <dinarysf276@gmail.com>
2025-12-31 08:17:48 +0700

    Update pissel.sh

--- pissel.sh
+++ pissel.sh
@@ -8,7 +8,7 @@ git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b 
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-23.0 vendor/xiaomi/earth
 git clone https://github.com/SilverEuphonium/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
 
-git clone https://github.com/SilverEuphonium/android_hardware_xiaomi.git -b lineage-23.1 hardware/xiaomi
+git clone https://github.com/SilverEuphonium/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr -b lineage-23.0 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/priv_keys.git -b main vendor/lineage-priv/keys
