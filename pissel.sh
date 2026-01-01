ef6cf3cb89a5c5bd31561ad63982a78ca9d674d8
Kumiko <dinarysf276@gmail.com>
2026-01-01 15:58:24 +0700

    Update pissel.sh

--- pissel.sh
+++ pissel.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
 
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.1 --git-lfs
+repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen-qpr1 --git-lfs
 /opt/crave/resync.sh || repo sync
 
-git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b Axion-16.1 device/xiaomi/earth
+git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b PixelOS-16.1 device/xiaomi/earth
 git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23.1 vendor/xiaomi/earth
 git clone https://github.com/SilverEuphonium/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
 
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
+git clone https://github.com/SilverEuphonium/android_hardware_xiaomi.git -b lineage-23.1 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
 git clone https://github.com/SilverEuphonium/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/priv_keys.git -b main vendor/lineage-priv/keys
@@ -18,5 +18,5 @@ export BUILD_USERNAME=oumae
 export BUILD_HOSTNAME=euphonium 
 
 . build/envsetup.sh
-axion earth userdebug vanilla
-ax -br
+breakfast earth userdebug
+m pixelos
