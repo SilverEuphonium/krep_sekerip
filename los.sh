c00175aa15f6fa573a047074f31eee1a5f2ebf63
Kumiko <dinarysf276@gmail.com>
2026-01-03 11:48:27 +0700

    Update and rename cr.sh to los.sh

--- los.sh
+++ los.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
 
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.1 --git-lfs
 /opt/crave/resync.sh || repo sync
 
-git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b Infinity-16.1 device/xiaomi/earth
+git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b lineage-23.1 device/xiaomi/earth
 git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23.1 vendor/xiaomi/earth
 git clone https://github.com/SilverEuphonium/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
 
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.1 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.1 hardware/mediatek
 git clone https://github.com/SilverEuphonium/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/priv_keys.git -b infinity vendor/infinity-priv/keys
 git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
@@ -18,5 +18,5 @@ export BUILD_USERNAME=oumae
 export BUILD_HOSTNAME=euphonium 
 
 . build/envsetup.sh
-lunch infinity_earth-userdebug
+lunch lineage_earth-bp3a-userdebug
 mka bacon 
