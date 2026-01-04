5e3dffc009c532e69e4008fc2c17bb263df0afc1
Kumiko <dinarysf276@gmail.com>
2026-01-04 13:34:56 +0700

    Update los.sh

--- los.sh
+++ los.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
 
-repo init -u https://github.com/LineageOS/android.git -b lineage-23.1 --git-lfs
+repo init -u https://github.com/SilverEuphonium/manifest.git -b 16-qpr2 --git-lfs
 /opt/crave/resync.sh || repo sync
 
-git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b lineage-23.1 device/xiaomi/earth
+git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b Mica-16.2 device/xiaomi/earth
 git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23.1 vendor/xiaomi/earth
 git clone https://github.com/SilverEuphonium/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
 
@@ -18,5 +18,5 @@ export BUILD_USERNAME=ririsaa
 export BUILD_HOSTNAME=oumae
 
 . build/envsetup.sh
-lunch lineage_earth-bp3a-userdebug
+lunch mica_earth-bp4a-userdebug
 mka bacon 
