3e0853051053df48dee175476eae9b9b0e225228
Kumiko <dinarysf276@gmail.com>
2026-01-04 16:38:37 +0700

    Update los.sh

--- los.sh
+++ los.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-rm -rf prebuilts/clang/host/linux-x86
+# rm -rf prebuilts/clang/host/linux-x86
 
-repo init -u https://github.com/SilverEuphonium/manifest.git -b 16-qpr2 --git-lfs
-/opt/crave/resync.sh || repo sync
+# repo init -u https://github.com/SilverEuphonium/manifest.git -b 16-qpr2 --git-lfs
+# /opt/crave/resync.sh || repo sync
 
 git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b Mica-16.2 device/xiaomi/earth
 git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23.1 vendor/xiaomi/earth
@@ -19,4 +19,4 @@ export BUILD_HOSTNAME=oumae
 
 . build/envsetup.sh
 lunch mica_earth-bp4a-userdebug
-mka bacon 
+m mica-release 
