4cf9181f9d82993051d5c5252242db9a967a5c5c
Kumiko <dinarysf276@gmail.com>
2026-01-06 05:46:33 +0700

    Update and rename mika.sh to los.sh

---
+++ los.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+rm -rf prebuilts/clang/host/linux-x86
+
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
+/opt/crave/resync.sh || repo sync
+
+git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b lineage-23.1 device/xiaomi/earth
+git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23.1 vendor/xiaomi/earth
+git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
+
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.1 hardware/mediatek
+git clone https://github.com/SilverEuphonium/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
+git clone https://github.com/AbuRider/priv_keys.git -b main vendor/lineage-priv/keys
+git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
+
+export BUILD_USERNAME=ririsaa
+export BUILD_HOSTNAME=oumae
+
+. build/envsetup.sh
+lunch lineage_earth-bp4a-userdebug
+mka bacon
