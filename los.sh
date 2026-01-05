0ebef1c8835f90d7dc6a8bb0eeab78539f4d4d8f
Kumiko <dinarysf276@gmail.com>
2026-01-05 07:18:31 +0700

    Update los.sh

--- los.sh
+++ los.sh
@@ -5,13 +5,13 @@
 # /opt/crave/resync.sh || repo sync
 
 # git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b Mica-16.2 device/xiaomi/earth
-# git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23.1 vendor/xiaomi/earth
+rm -rf vendor/xiaomi/earth
+git clone https://github.com/SilverEuphonium/proprietary_vendor_xiaomi_earth.git -b lineage-23.2 vendor/xiaomi/earth
 # git clone https://github.com/SilverEuphonium/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
 
 # git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.1 hardware/xiaomi
 # git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.1 hardware/mediatek
-rm -rf device/mediatek/sepolicy_vndr
-git clone https://github.com/SilverEuphonium/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
+# git clone https://github.com/SilverEuphonium/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
 # git clone https://github.com/AbuRider/priv_keys.git -b main vendor/lineage-priv/keys
 # git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
 
