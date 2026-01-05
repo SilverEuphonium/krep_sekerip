0df380efa0ecddef7d6c6616f172f9e5f1cd3968
Kumiko <dinarysf276@gmail.com>
2026-01-05 13:17:49 +0700

    Update mika.sh

--- mika.sh
+++ mika.sh
@@ -8,7 +8,7 @@ git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b 
 git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23.1 vendor/xiaomi/earth
 git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
 
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.1 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.1 hardware/mediatek
 git clone https://github.com/SilverEuphonium/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/priv_keys.git -b master vendor/lineage-priv/keys
