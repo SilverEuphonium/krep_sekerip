24e18a378240d6314cf20d6685e0af55354af08c
Kumiko <dinarysf276@gmail.com>
2026-01-20 22:39:45 +0700

    Update evo.sh

--- evo.sh
+++ evo.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
+rm -rf device/xiaomi/earth kernel/xiaomi/earth vendor/xiaomi/earth
+rm -rf hardware/xiaomi hardware/mediatek device/mediatek/sepolicy_vndr vendor/os-priv/keys
 
-repo init -u https://github.com/Evolution-X/manifest -b bq1 --git-lfs 
+repo init -u https://github.com/Evolution-X/manifest -b udc --git-lfs 
 /opt/crave/resync.sh || repo sync
 
-git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b EvolutionX-16.1 device/xiaomi/earth
-git clone https://github.com/AbuRider/proprietary_vendor_xiaomi_earth.git -b lineage-23.1 vendor/xiaomi/earth
-git clone https://github.com/SilverEuphonium/android_kernel_xiaomi_earth.git -b 16 kernel/xiaomi/earth
+git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b EvolutionX-14 device/xiaomi/earth
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-21 vendor/xiaomi/earth
+git clone https://github.com/AbuRider/android_kernel_bumi.git -b 4.19.306 kernel/xiaomi/earth
 
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.0 hardware/mediatek
-git clone https://github.com/AbuRider/android_device_mediatek_sepolicy_vndr.git -b lineage-23.0 device/mediatek/sepolicy_vndr
-git clone https://github.com/AbuRider/priv_keys.git -b evox vendor/evolution-priv/keys
-git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-21 device/mediatek/sepolicy_vndr
+git clone https://github.com/AbuRider/sign.git -b evok vendor/evolution-priv/keys
 
 # Export
 export BUILD_USERNAME=kumiko
@@ -21,5 +22,5 @@ export ALLOW_MISSING_DEPENDENCIES=true
 
 # starts build setup !
 . build/envsetup.sh
-lunch lineage_earth-bp3a-userdebuolqg
+lunch lineage_earth-userdebug
 m evolution 
