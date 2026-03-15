fd1938aed7c953162d1a1528874ba5a922a13d23
Kumiko <dinarysf276@gmail.com>
2026-03-15 22:34:24 +0700

    Update evo.sh

--- evo.sh
+++ evo.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 rm -rf prebuilts/clang/host/linux-x86
-rm -rf device/xiaomi/earth kernel/xiaomi/earth vendor/xiaomi/earth
-rm -rf hardware/xiaomi hardware/mediatek device/mediatek/sepolicy_vndr vendor/os-priv/keys
 
-repo init -u https://github.com/Evolution-X/manifest -b udc --git-lfs 
+repo init -u https://github.com/Evolution-X/manifest.git -b bq2 --git-lfs
 /opt/crave/resync.sh || repo sync
 
-git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b EvolutionX-14 device/xiaomi/earth
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_earth.git -b lineage-21 vendor/xiaomi/earth
-git clone https://github.com/AbuRider/android_kernel_bumi.git -b 4.19.306 kernel/xiaomi/earth
+git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b EvolutionX-16.2 device/xiaomi/earth
+git clone https://github.com/AbuRider/vendor_xiaomi_earth.git -b 16.2 vendor/xiaomi/earth
+git clone https://github.com/AbuRider/android_kernel_xiaomi_earth.git -b 16.2 kernel/xiaomi/earth
 
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-21 device/mediatek/sepolicy_vndr
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-23.2 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-23.2 device/mediatek/sepolicy_vndr
 git clone https://github.com/AbuRider/sign.git -b evok vendor/evolution-priv/keys
+git clone https://github.com/MillenniumOSS/android_vendor_mediatek_ims.git -b sixteen-qpr2 vendor/mediatek/ims
 
-# Export
-export BUILD_USERNAME=megumi
-export BUILD_HOSTNAME=wind_orchestra
-export TZ="Asia/Jakarta"
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export RELAX_USES_LIBRARY_CHECK=true
+export BUILD_USERNAME=kumiko
+export BUILD_HOSTNAME=clarinet_quartet
 
-# starts build setup !
 . build/envsetup.sh
-lunch lineage_earth-userdebug
-make installclean
-m evolution 
+lunch lineage_earth-bp4a-userdebug
+m evolution
