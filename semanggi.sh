cf89ca4adc9cb9d965385b9bc47fe2555c22cf40
Kumiko <dinarysf276@gmail.com>
2026-04-21 05:20:32 +0700

    Update semanggi.sh

--- semanggi.sh
+++ semanggi.sh
@@ -4,7 +4,8 @@
 # repo init -u https://github.com/The-Clover-Project/manifest.git -b 16-qpr2 --git-lfs 
 # /opt/crave/resync.sh || repo sync
 
-# git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth -b Clover-16.2 device/xiaomi/earth
+rm -rf device/xiaomi/earth
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth -b Clover-16.2 device/xiaomi/earth
 
 git clone https://github.com/LineageOS/android_packages_apps_EuiccPolicy.git -b lineage-23.2 packages/apps/EuiccPolicy
 
