83975bc24bfd70b6003a3794d7d0d63cbf80cb63
Kumiko <dinarysf276@gmail.com>
2026-04-21 14:07:11 +0700

    Update pissel.sh

--- pissel.sh
+++ pissel.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-rm -rf device/xiaomi/earth vendor/xiaomi/earth kernel/xiaomi/earth vendor/mediatek/ims
-rm -rf hardware/mediatek hardware/xiaomi device/mediatek/sepolicy_vndr
 
-git clone https://github.com/SilverEuphonium/android_device_xiaomi_earth.git -b PixelOS-16.2 device/xiaomi/earth
+repo init -u https://github.com/Kitauji-High-School/pixelos_manifest.git -b test --git-lfs
+/opt/crave/resync.sh || repo sync
+
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b PixelOS-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
