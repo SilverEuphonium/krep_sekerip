6f34985a821b11b544bfebfd270063cb820c4e63
Kumiko <dinarysf276@gmail.com>
2026-05-05 15:34:32 +0700

    Update los.sh

--- los.sh
+++ los.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-rm -rf prebuilts/clang/host/linux-x86
+rm -rf device/xiaomi/earth
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b lineage-23.2 device/xiaomi/earth
 
-repo init -u https://github.com/crdroidandroid/android -b 16.0 --git-lfs --no-clone-bundle
-/opt/crave/resync.sh || repo sync
-
-git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b crDroid-16.2 device/xiaomi/earth
+rm -rf packages/apps/crDroidSettings
+git clone https://github.com/SilverEuphonium/android_packages_apps_crDroidSettings -b 16.0 packages/apps/crDroidSettings
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
+export TARGET_ENABLE_BLUR=false
+export TARGET_DISABLE_MATLOG=true
 
 . build/envsetup.sh
-brunch earth userdebug
+lunch lineage_earth-bp4a-userdebug
+make installclean
+mka bacon
