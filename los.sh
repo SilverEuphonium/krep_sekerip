45f78df3c281fc9a3c1e0a5556c14700496ba5d5
Kumiko <dinarysf276@gmail.com>
2026-05-21 05:17:31 +0700

    Update los.sh

--- los.sh
+++ los.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-repo init -u https://github.com/SilverEuphonium/android_manifest.git -b 16.0 --git-lfs --no-clone-bundle
-/opt/crave/resync.sh || repo sync
-
-git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b crDroid-16.2 device/xiaomi/earth
-
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
