4c8b2b6952f9d16fe02efb4b78008e12f8929cf2
Kumiko <dinarysf276@gmail.com>
2026-05-21 01:26:54 +0700

    Update los.sh

--- los.sh
+++ los.sh
@@ -12,3 +12,11 @@ export BUILD_HOSTNAME=kitauji_quartet
 lunch lineage_earth-bp4a-userdebug
 make installclean
 mka bacon
+
+# Upload to gofile
+echo "Upload to gofile will be started..."
+if [ -f out/target/product/earth/*.zip ]; then
+    wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
+    chmod +x upload.sh ; ./upload.sh out/target/product/earth/*.zip
+fi
+echo "hame"
