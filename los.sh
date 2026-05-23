996ce81fb60f3d49451a6fdb4d8277eb9370e409
Kumiko <dinarysf276@gmail.com>
2026-05-23 23:11:48 +0700

    Update los.sh

--- los.sh
+++ los.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+rm -rf prebuilts/clang/host/linux-x86
+
+repo init -u https://github.com/SilverEuphonium/android_manifest.git -b 16.0 --git-lfs --no-clone-bundle
+/opt/crave/resync.sh # sync source
+
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b crDroid-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
 . build/envsetup.sh
 lunch lineage_earth-bp4a-userdebug
-make installclean
 mka bacon
 
 # Upload to gofile
