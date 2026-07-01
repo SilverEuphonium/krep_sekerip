15ab109ab95adda1762fec7d5ca7a77337a8e139
Grayfia <dinarysf276@gmail.com>
2026-07-01 16:03:04 +0700

    Update los.sh

--- los.sh
+++ los.sh
@@ -10,12 +10,13 @@ export BUILD_HOSTNAME=kitauji_quartet
 
 . build/envsetup.sh
 lunch lineage_earth-bp4a-userdebug
+make installclean
 mka bacon
 
 # Upload to gofile
 echo "Upload to gofile will be started..."
 if [ -f out/target/product/earth/*.zip ]; then
     wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
-    chmod +x upload.sh ; ./upload.sh out/target/product/earth/*.zip
+    chmod +x upload.sh ; ./upload.sh out/target/product/earth/*202607*.zip
 fi
 echo "hame"
